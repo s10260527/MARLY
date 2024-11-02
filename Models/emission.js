298242c0e775744a20b281fd57dc8397474a2cc3
Toh-Yang-Yi <S10258639@connect.np.edu.sg>
2024-11-02 16:20:00 +0800

    feat: created route to get all emission from current month

--- Models/emission.js
+++ Models/emission.js
@@ -2,12 +2,12 @@ const sql = require("mssql");
 const dbConfig = require("../dbConfig");
 
 class Emission {
-    constructor(emission_id, company_id, emission_type, period_id, amount) {
+    constructor(emission_id, company_id, emission_year, emission_amount, created_at) {
         this.emission_id = emission_id;
         this.company_id = company_id;
-        this.emission_type = emission_type;
-        this.period_id = period_id;
-        this.amount = amount;
+        this.emission_year = emission_year;
+        this.emission_amount = emission_amount;
+        this.created_at = created_at;
     }
 
     static async getEmissionById(id) {
@@ -27,11 +27,36 @@ class Emission {
                 ? new Emission(
                     emission.emission_id,
                     emission.company_id,
-                    emission.emission_type,  // This will now directly show the type
-                    emission.period_id,
-                    emission.amount
+                    emission.emission_year,
+                    emission.emission_amount,
+                    emission.created_at
                   )
                 : null;
+        } catch (error) {
+            console.error(error);
+            throw new Error("Database error");
+        }
+    }
+
+    static async getAllEmissionByCurrentMonth() {
+        try {
+            await sql.connect(dbConfig);
+
+            const sqlQuery = `SELECT * FROM Emissions WHERE MONTH(created_at) = MONTH(GETDATE()) AND YEAR(created_at) = YEAR(GETDATE());`;
+
+            const request = new sql.Request();
+            const result = await request.query(sqlQuery);
+
+            sql.close();
+
+            // Map the result to an array of Emission instances
+            return result.recordset.map(emission => new Emission(
+                emission.emission_id,
+                emission.company_id,
+                emission.emission_year,
+                emission.emission_amount,
+                emission.created_at
+            ));
         } catch (error) {
             console.error(error);
             throw new Error("Database error");
