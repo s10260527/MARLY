fa69719cd4311a98a973f1a95233af62eb6b8443
AqeilDanish <s10257689@connect.np.edu.sg>
2024-11-07 15:39:41 +0800

    Merged main and Aqeil

--- Models/emission.js
+++ Models/emission.js
@@ -106,6 +106,46 @@ class Emission {
             throw new Error("Database error");
         }
     }
+     // CREATE a new emission record
+     static async createEmission(emissionData) {
+        try {
+            await sql.connect(dbConfig);
+            const sqlQuery = `
+                INSERT INTO Emissions (company_id, emission_year, emission_amount, created_at)
+                VALUES (@company_id, @emission_year, @emission_amount, GETDATE())
+            `;
+            const request = new sql.Request();
+            request.input("company_id", sql.Int, emissionData.company_id);
+            request.input("emission_year", sql.Int, emissionData.emission_year);
+            request.input("emission_amount", sql.Float, emissionData.emission_amount);
+            await request.query(sqlQuery);
+            sql.close();
+        } catch (error) {
+            console.error(error);
+            throw new Error("Error creating emission record");
+        }
+    }
+    // UPDATE an existing emission record
+    static async updateEmission(id, updatedData) {
+        try {
+            await sql.connect(dbConfig);
+            const sqlQuery = `
+                UPDATE Emissions 
+                SET emission_amount = @emission_amount, emission_year = @emission_year
+                WHERE emission_id = @emission_id
+            `;
+            const request = new sql.Request();
+            request.input("emission_id", sql.Int, id);
+            request.input("emission_year", sql.Int, updatedData.emission_year);
+            request.input("emission_amount", sql.Float, updatedData.emission_amount);
+            await request.query(sqlQuery);
+            sql.close();
+        } catch (error) {
+            console.error(error);
+            throw new Error("Error updating emission record");
+        }
+    }
+    
 }
 
 module.exports = Emission;
