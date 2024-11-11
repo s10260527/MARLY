de038080fbcfadb40b1ed90bce80043d9e670399
Aqeil Danish <s10257689@connect.np.edu.sg>
2024-11-11 23:20:46 +0800

    fixed app.js

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const sql = require("mssql");
+const dbConfig = require("./dbConfig");
+const cors = require("cors");
+const signupRouter = require('./Models/signup');
+const emissioncontroller = require("./Controllers/emission");
+
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+// Serve static files (HTML, CSS, JS) from the "Public" directory
+app.use(express.static("Public"));
+
+// Define a route to serve the signup page
+app.get("/signup", (req, res) => {
+    res.sendFile(__dirname + "/Public/signup.html");
+});
+
+// API routes
+app.use('/api', signupRouter);
+app.get("/emission/totalemission", emissioncontroller.getTopEmissionsByCurrentMonth);
+app.get("/emission/mostimproved", emissioncontroller.getMostImprovedByMonth);
+
+app.listen(port, async () => {
+    try {
+        await sql.connect(dbConfig);
+        console.log("Database connection success");
+    } catch (err) {
+        console.error("Database connection error", err);
+        process.exit(1);
+    }
+    console.log(`Server listening on port ${port}`);
+});
+
+process.on("SIGINT", async () => {
+    console.log("Server shutting down gracefully");
+    await sql.close();
+    console.log("Database connection closed");
+    process.exit(0);
+});
